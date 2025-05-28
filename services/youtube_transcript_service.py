68575c4edca14911a294050a8bef00e435cbe1a6
Dilip Ghimire <Dilipghimire2@gmail.com>
2025-05-28 10:41:36 -0400

    add proxy

--- services/youtube_transcript_service.py
+++ services/youtube_transcript_service.py
@@ -1,10 +1,10 @@
+import os
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 
 
 def fetch_transcript (video_id: str) -> str:
     proxies = {
-        "https":"http://45.12.150.82:8080",
-        "https":"57.129.81.201:8080",
+       "http": os.getenv("PROXY")
         }
     transcript_list = YouTubeTranscriptApi.get_transcript(video_id, languages=["en"], proxies=proxies)
     return " ".join(chunk["text"] for chunk in transcript_list)
