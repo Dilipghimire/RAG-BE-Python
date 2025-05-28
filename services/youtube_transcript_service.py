5e5ef7922ce2dfd5ddea582cdf0804cbfdfe6250
Dilip Ghimire <Dilipghimire2@gmail.com>
2025-05-28 10:53:15 -0400

    print prox

--- services/youtube_transcript_service.py
+++ services/youtube_transcript_service.py
@@ -7,5 +7,6 @@ def fetch_transcript (video_id: str) -> str:
        "http": os.getenv("PROXY"),
        "https": os.getenv("PROXY")
         }
+    print('proxies', proxies)
     transcript_list = YouTubeTranscriptApi.get_transcript(video_id, languages=["en"], proxies=proxies)
     return " ".join(chunk["text"] for chunk in transcript_list)
