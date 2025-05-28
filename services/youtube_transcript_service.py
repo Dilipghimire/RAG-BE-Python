b8c95516c374ace759b2d23ee2f2708270b2e387
Dilip Ghimire <Dilipghimire2@gmail.com>
2025-05-28 12:44:47 -0400

    remove timeout

--- services/youtube_transcript_service.py
+++ services/youtube_transcript_service.py
@@ -10,5 +10,5 @@ def fetch_transcript (video_id: str) -> str:
        "https": os.getenv("PROXY")
         }
     print('proxies', proxies)
-    transcript_list = YouTubeTranscriptApi.get_transcript(video_id, languages=["en"], proxies=proxies, timeout=10)
+    transcript_list = YouTubeTranscriptApi.get_transcript(video_id, languages=["en"], proxies=proxies)
     return " ".join(chunk["text"] for chunk in transcript_list)
